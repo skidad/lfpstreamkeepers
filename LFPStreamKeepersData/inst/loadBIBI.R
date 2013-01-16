2488a099ee7185223fbbdd36266b5605705e3a04
derek <skidad60@gmail.com>
2013-01-15 21:41:40 -0800

    setting up loading all BIBI

---
+++ LFPStreamKeepersData/inst/loadBIBI.R
@@ -0,0 +1,30 @@
+library( LFPStreamKeepersData )
+
+lyonFiene2006 <- readScoringXls( "extdata/BIBI/2006/Lyon_at_fiene_scoring_2006.xls")
+lyonFiene2007 <- readScoringXls( "extdata/BIBI/2007/Lyon_Creek_Fiene_Scoring_2007.xls" )
+
+2008???
+
+2009 <- readScoringXls( "extdata/BIBI/2009/Lyon\ Creek\ at\ 35th\ Ave\ Scoring\ 2009.xls" )
+2009 <- readScoringXls( "extdata/BIBI/2009/Lyon\ Creek\ at\ Fienes\ Scoring\ 2009.xls" )
+2009 <- readScoringXls( "extdata/BIBI/2009/McAleer\ Creek\ at\ Animal\ Acres\ Scoring\ 2009.xls" )
+2009 <- readScoringXls( "extdata/BIBI/2009/McAleer\ Creek\ at\ Perkins\ Scoring\ 2009.xls" )
+
+2009 <- readScoringXls( "extdata/BIBI/2009/Lyon\ Creek\ at\ 35th\ Ave\ Scoring\ 2009.xls" )
+2009 <- readScoringXls( "extdata/BIBI/2009/Lyon\ Creek\ at\ Fienes\ Scoring\ 2009.xls" )
+2009 <- readScoringXls( "extdata/BIBI/2009/McAleer\ Creek\ at\ Animal\ Acres\ Scoring\ 2009.xls" )
+2009 <- readScoringXls( "extdata/BIBI/2009/McAleer\ Creek\ at\ Perkins\ Scoring\ 2009.xls" )
+
+
+2010 <- readScoringXls( "extdata/BIBI/2010/Lyon Creek at 35th Ave Scoring 2010.xls" )
+2010 <- readScoringXls( "extdata/BIBI/2010/Lyon Creek at Fienes Scoring 2010.xls" )
+2010 <- readScoringXls( "extdata/BIBI/2010/McAleer Creek at Animal Acres Scoring 2010.xls" )
+2010 <- readScoringXls( "extdata/BIBI/2010/McAleer Creek at Perkins Scoring 2010.xls" )
+
+2012 <- readScoringXls( "extdata/BIBI/2012/Animal_Acres_2012_Scoring.xlsx" )
+2012 <- readScoringXls( "extdata/BIBI/2012/Lyons Creek at 178th 2012 Scoring.xlsx" )
+2012 <- readScoringXls( "extdata/BIBI/2012/Lyons Creek at 35th 2012 Scoring.xlsx" )
+2012 <- readScoringXls( "extdata/BIBI/2012/perkins way 2012 Scoring.xlsx" )
+
+#can we look at 2011 data and see if it is showing same stuff?
+
