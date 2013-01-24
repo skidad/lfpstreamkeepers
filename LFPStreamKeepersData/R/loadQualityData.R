55f340fd5c34d51b00cbea1738656131e74cd1e9
derek <skidad60@gmail.com>
2013-01-23 22:33:53 -0800

    Added rawQualityData.csv, skeliton of data set documentation. rawQualityData.csv needs cleaning: read.table doesn't like it.

---
+++ LFPStreamKeepersData/R/loadQualityData.R
@@ -0,0 +1,5 @@
+
+loadQualityData <- function( ){
+    file.name<-system.file( "extdata/rawQualityData.csv", package="LFPStreamKeepersData")
+	df <- read.table( file.name,header=FALSE,sep=",")
+}
--- LFPStreamKeepersData/inst/SetupAndRun.R
+++ LFPStreamKeepersData/inst/SetupAndRun.R
@@ -17,6 +17,8 @@ odbcClose(excel.connect)
 
 lyonFiene2006 <- readScoringXls( "extdata/BIBI/2006/Lyon_at_fiene_scoring_2006.xls")
 lyonFiene2007 <- readScoringXls( "extdata/BIBI/2007/Lyon_Creek_Fiene_Scoring_2007.xls" )
+##################
+qd <- loadQualityData()
 
 ##################
 library(ggplot2)
